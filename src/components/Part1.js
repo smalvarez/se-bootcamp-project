e7dde9476ae6eaf112595ec2bdf5694ac0f0db07
Steven Alvarez <smalvarez#gmail.com>
2024-06-11 11:53:04 -0400

    Merge with unrelated histories

--- src/components/Part1.js
+++ src/components/Part1.js
@@ -1,15 +1,14 @@
 // src/components/Part1.js
 import React, { useState } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Signup from "./Signup";
+import SignupModal from "./SignupModal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/part2.css";
 
 function Part1() {
   const [modalState, setModalState] = useState({ currentModal: null });
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   const openModal = (modalId) => {
     setModalState({ currentModal: modalId });
@@ -19,33 +18,16 @@ function Part1() {
     setModalState({ currentModal: null });
   };
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        "https://se-bootcamp-project.stevenalvarez.me/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-        }
-      );
-
-      const data = await response.json();
-
-      if (response.ok) {
-        localStorage.setItem("token", data.token);
-        alert("Login successful");
-        closeModal();
-      } else {
-        alert(data.message);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("An error occurred. Please try again.");
-    }
+  const openSignupWindow = () => {
+    const width = 500;
+    const height = 600;
+    const left = window.screen.width / 2 - width / 2;
+    const top = window.screen.height / 2 - height / 2;
+    window.open(
+      "/signup.html",
+      "Signup",
+      `width=${width},height=${height},top=${top},left=${left}`
+    );
   };
 
   return (
@@ -69,45 +51,19 @@ function Part1() {
                 >
                   Login
                 </Button>
-                <Link to="/signup">
-                  <Button>Sign Up</Button>
-                </Link>
+                <Button className="mr-2" onClick={openSignupWindow}>
+                  Sign Up
+                </Button>
               </div>
             </div>
           </section>
         </main>
 
-        <Modal
+        <SignupModal
           show={modalState.currentModal === "loginModal"}
           onHide={closeModal}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Login</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={handleLogin}>
-              <Form.Group>
-                <Form.Control
-                  type="email"
-                  placeholder="Email Address*"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Control
-                  type="password"
-                  placeholder="Password*"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit" block>
-                LOGIN
-              </Button>
-            </Form>
-          </Modal.Body>
-        </Modal>
+          type="login"
+        />
 
         <Routes>
           <Route path="/signup" element={<Signup />} />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
+import SignupPage from "./components/SignupPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="*" element={<App />} />
+    </Routes>
+  </Router>,
   document.getElementById("root")
 );
+
+// Render the signup page if on signup.html
+if (document.getElementById("signup-root")) {
+  ReactDOM.render(<SignupPage />, document.getElementById("signup-root"));
+}
