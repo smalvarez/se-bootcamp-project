5851d6e6d0fbb1f8283701f6e82df6426b43a27d
smalvarez <smalvarez@users.noreply.github.com>
2024-06-11 17:38:50 +0000

    deploy: b0cc09bd2e9e1f3ba070d995ee19b5b724517b15

---
+++ precache-manifest.1cde04d81f2753efc9a1cc1ca59e911e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ee18ceb2660d4f01af390e6b7e6cd5",
+    "url": "/se-bootcamp-project/index.html"
+  },
+  {
+    "revision": "0bb6af838d57bd54aa03",
+    "url": "/se-bootcamp-project/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "c5974004598a40cc9eb8",
+    "url": "/se-bootcamp-project/static/css/main.6bb02eba.chunk.css"
+  },
+  {
+    "revision": "0bb6af838d57bd54aa03",
+    "url": "/se-bootcamp-project/static/js/2.a72ef828.chunk.js"
+  },
+  {
+    "revision": "14e95d2b5ba792183a4f7cdb5ae25ff8",
+    "url": "/se-bootcamp-project/static/js/2.a72ef828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5974004598a40cc9eb8",
+    "url": "/se-bootcamp-project/static/js/main.d4afccca.chunk.js"
+  },
+  {
+    "revision": "c3d7ebdea48da6bab88b",
+    "url": "/se-bootcamp-project/static/js/runtime-main.06101bc9.js"
+  }
+]);
