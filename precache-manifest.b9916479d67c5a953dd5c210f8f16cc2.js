55b76648b9a93e29a88405922b0906a15f0ce308
smalvarez <smalvarez@users.noreply.github.com>
2024-06-11 18:29:29 +0000

    deploy: 36d60af03f011c5cd931e2e454f30d1c18293fa8

---
+++ precache-manifest.b9916479d67c5a953dd5c210f8f16cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff4aa23efa4af2a2d2025117efaa0cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0b724d08cdfcd0220c",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "45f8be45252473ddf457",
+    "url": "/static/css/main.6bb02eba.chunk.css"
+  },
+  {
+    "revision": "3d0b724d08cdfcd0220c",
+    "url": "/static/js/2.c8db96a9.chunk.js"
+  },
+  {
+    "revision": "6cb2abd69bcf5c1bc8ea13bc6a30ce0e",
+    "url": "/static/js/2.c8db96a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f8be45252473ddf457",
+    "url": "/static/js/main.6ea30e2e.chunk.js"
+  },
+  {
+    "revision": "01b5036475f347f5254f",
+    "url": "/static/js/runtime-main.4a87ca57.js"
+  }
+]);
---
+++ src/components/SignUpForm.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
+function SignUpForm({ show, handleClose }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://se-bootcamp-project.stevenalvarez.me/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Signup successful");
+        handleClose();
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Sign Up</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Email Address*"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password*"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password*"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button variant="secondary" type="submit" block>
+            SIGN UP
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default SignUpForm;
