f63046c3625d62f7b42cf79eb3fcee7368d382be
smalvarez <smalvarez@users.noreply.github.com>
2024-06-11 17:21:35 +0000

    deploy: d1229a1df38d2d6881b345635a3af7173b9531bb

---
+++ test_db_connection.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectTimeout: 10000, // 10 seconds
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+    return;
+  }
+  console.log("Connected to the database as id " + connection.threadId);
+});
+
+connection.end();
