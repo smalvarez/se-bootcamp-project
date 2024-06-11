dab43bb68d6c9ab4f22f402a747f9727673d5fb3
Steven Alvarez <smalvarez#gmail.com>
2024-06-11 14:03:08 -0400

    Updates

--- src/MutationObserver.js
+++
@@ -1,8 +0,0 @@
-// src/mutationObserver.js
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    console.log(mutation);
-  });
-});
-
-observer.observe(document.body, { childList: true, subtree: true });
