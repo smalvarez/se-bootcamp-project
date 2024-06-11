8b7b5ea252feeebdb5a3dcd5700f4e5417a566f7
smalvarez <smalvarez@users.noreply.github.com>
2024-06-11 18:17:35 +0000

    deploy: 9c1306b2ec22cdc977afb77144897c71c5f8a79e

---
+++ src/components/Signup.js
@@ -0,0 +1,66 @@
+// src/components/Signup.js
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://smalvarez.github.io/se-bootcamp-project/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Signup successful");
+        // Redirect or close modal
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className="container my-4">
+      <h2>Sign Up</h2>
+      <Form onSubmit={handleSignUp}>
+        <Form.Group>
+          <Form.Control
+            type="email"
+            placeholder="Email Address*"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type="password"
+            placeholder="Password*"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" block>
+          SIGN UP
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Signup;
